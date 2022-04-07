d54e74d6b334964db3ad2654f5a8cddcd6ace9ac
Karl Svartholm <gorbiz@gmail.com>
2022-04-07 18:34:39 +0200

    :shirt: lint

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-import './style.css';
-import GeoJSON from 'ol/format/GeoJSON';
-import {Map, View} from 'ol';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import './style.css'
+import GeoJSON from 'ol/format/GeoJSON'
+import { Map, View } from 'ol'
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
 
 import {
   Circle as CircleStyle,
   Fill,
   Stroke,
   Style,
-  Text,
-} from 'ol/style';
-import { OSM, XYZ, Vector as VectorSource } from 'ol/source';
+  Text
+} from 'ol/style'
+import { OSM, XYZ, Vector as VectorSource } from 'ol/source'
 
 const image = new CircleStyle({
   radius: 5,
@@ -19,42 +19,42 @@ const image = new CircleStyle({
 })
 
 const styles = {
-  'Point': new Style({ image })
+  Point: new Style({ image })
 }
 
 const styleFunction = function (feature) {
   return styles[feature.getGeometry().getType()]
-};
+}
 
 const geojsonObject = {
-  'type': 'FeatureCollection',
-  'crs': {
-    'type': 'name',
-    'properties': {
-      'name': 'EPSG:3857',
-    },
+  type: 'FeatureCollection',
+  crs: {
+    type: 'name',
+    properties: {
+      name: 'EPSG:3857'
+    }
   },
-  'features': [
+  features: [
     {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [4e5, -5e6],
-      },
-    },
-  ],
-};
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [4e5, -5e6]
+      }
+    }
+  ]
+}
 
 const vectorSource = new VectorSource({
-  features: new GeoJSON().readFeatures(geojsonObject),
-});
+  features: new GeoJSON().readFeatures(geojsonObject)
+})
 
 // vectorSource.addFeature(new Feature(new Circle([5e6, 7e6], 1e6)));
 
 const vectorLayer = new VectorLayer({
   source: vectorSource,
-  style: styleFunction,
-});
+  style: styleFunction
+})
 
 const map = new Map({
   layers: [
@@ -65,11 +65,11 @@ const map = new Map({
         maxZoom: 19
       })
     }),
-    vectorLayer,
+    vectorLayer
   ],
   target: 'map',
   view: new View({
     center: [0, 0],
-    zoom: 2,
-  }),
-});
+    zoom: 2
+  })
+})

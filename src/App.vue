2df6a72c6dfe3faece20163ae8fbb3accfba34a1
Karl Svartholm <gorbiz@gmail.com>
2022-04-08 16:43:08 +0200

    add separate timeline component, I think

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import TimeLine from './components/TimeLine.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <p>Test123</p>
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <TimeLine msg="hello" />
 </template>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,50 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue' // vue Related methods
+import { Map, View } from 'ol' // Map instance method 、 View method,
+import Tile from 'ol/layer/Tile' // Tile rendering method
+import OSM from 'ol/source/OSM' // OSM tiles 【OSM Cannot be used in actual development , because OSM There's something wrong with the border of China map in ！！！！】
+import 'ol/ol.css' // Map style
+
+import settlements from '../settlements'
+console.log({ settlements })
 
 defineProps({
   msg: String
 })
 
 const count = ref(0)
+
+const map = ref(null) // Store map instances
+function initMap() {
+  // Examples of maps
+  map.value = new Map({
+    target: 'map', // In the corresponding page id by map The elements of
+    layers: [ // Layers
+      new Tile({ // Using tile rendering methods
+        source: new OSM() // Layer data source
+      })
+    ],
+    view: new View({ // Map view
+      projection: "EPSG:4326", // Coordinate system , Yes EPSG:4326 and EPSG:3857
+      center: [114.064839, 22.548857], // Shenzhen coordinates
+      minZoom: 10, // Map zoom minimum level
+      zoom: 12 // Map zoom level （ The default level when opening a page ）
+    })
+  })
+}
+
+onMounted(() => {
+  initMap()
+})
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div id="map" class="map__x"></div>
 </template>
-
 <style scoped>
-a {
-  color: #42b983;
+.map__x {
+  width: 600px;
+  height: 600px;
+  border: 1px solid #eee;
 }
 </style>
